2dfbfc7a1f9dc3c76628a0ad4efd2bf70ca7b2c4
Laksshmiweb <lakshmiweb55@gmail.com>
2024-03-15 21:19:44 +0530

    add js file

---
+++ index.js
@@ -0,0 +1,10 @@
+let names=['jone','mike','Thara','stave','merry']
+// console.log(names)
+// let arrayslicename=names.slice(2,3)
+// console.log(arrayslicename);
+console.log(names.slice(2,-2))
+console.log(names.slice(-2))
+
+for(let name of names){
+    console.log('The student names is' +name)
+}
